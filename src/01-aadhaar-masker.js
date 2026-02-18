46550601aec697391513b19e2e87758a643323c3
Saurav Singh <sauravcloud26@gmail.com>
2026-02-18 23:25:31 +0530

    aadhaar-masker complete

--- src/01-aadhaar-masker.js
+++ src/01-aadhaar-masker.js
@@ -29,4 +29,11 @@
  */
 export function maskAadhaar(aadhaarNumber) {
   // Your code here
+  if (
+    typeof aadhaarNumber !== "string" ||
+    aadhaarNumber.length != 12 ||
+    !/^\d{12}$/.test(aadhaarNumber)
+  )
+    return "INVALID";
+  return "XXXX-".repeat(2) + aadhaarNumber.slice(-4);
 }
